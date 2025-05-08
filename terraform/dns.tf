15afb0ef0fcd20ad8623a9d9e7a491ef6a733c7d
ag <agaudon@proton.me>
2025-05-08 16:27:08 -0230

    remove unnecessary file

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -3,11 +3,3 @@ data "cloudflare_zone" "misery_systems" {
     name = "misery.systems"
   }
 }
-
-resource "cloudflare_dns_record" "downward" {
-  zone_id = data.cloudflare_zone.misery_systems.zone_id
-  name    = "downward.misery.systems"
-  content = local.ingress_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
