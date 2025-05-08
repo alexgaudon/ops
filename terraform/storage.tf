3ade488c884089179e9cda627f59057e1815d535
ag <agaudon@proton.me>
2025-05-08 16:20:59 -0230

    Added nfs-csi

---
+++ terraform/storage.tf
@@ -0,0 +1,22 @@
+resource "helm_release" "csi_nfs" {
+  name      = "csi-driver-nfs"
+  namespace = "kube-system"
+
+  repository = "https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts"
+  chart      = "csi-driver-nfs"
+  version    = "v4.11.0"
+}
+
+resource "kubernetes_storage_class" "nfs" {
+  metadata {
+    name = "nfs-csi"
+  }
+
+  storage_provisioner = "nfs.csi.k8s.io"
+
+  parameters = {
+    server = "10.0.0.5"
+    share  = "/mnt/pool/k8s"
+    subDir = "$${pvc.metadata.namespace}/$${pvc.metadata.name}"
+  }
+}
