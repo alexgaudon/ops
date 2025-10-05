1fabd19de739b6752dd0511ef5ef69c274e26119
ag <agaudon@proton.me>
2025-10-05 12:38:32 -0230

    dns for misery.systems

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -4,6 +4,14 @@ data "cloudflare_zone" "misery_systems" {
   }
 }
 
+resource "cloudflare_dns_record" "misery_systems" {
+  zone_id = data.cloudflare_zone.misery_systems.zone_id
+  name    = "misery.systems"
+  content = local.ingress_hostname
+  type    = "CNAME"
+  ttl     = 1
+}
+
 resource "cloudflare_dns_record" "downward" {
   zone_id = data.cloudflare_zone.misery_systems.zone_id
   name    = "downward.misery.systems"
