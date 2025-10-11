7bb5c26dee1c2a498caa53e7c879ff62ce5661e5
ag <agaudon@proton.me>
2025-10-11 12:57:09 -0230

    remove downward

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -12,14 +12,6 @@ resource "cloudflare_dns_record" "misery_systems" {
   ttl     = 1
 }
 
-resource "cloudflare_dns_record" "downward" {
-  zone_id = data.cloudflare_zone.misery_systems.zone_id
-  name    = "downward.misery.systems"
-  content = local.ingress_hostname
-  type    = "CNAME"
-  ttl     = 1
-}
-
 resource "cloudflare_dns_record" "thirteenft" {
   zone_id = data.cloudflare_zone.misery_systems.zone_id
   name    = "13ft.misery.systems"
